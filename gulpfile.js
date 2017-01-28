98dd5439bb04992bf1206089b07ec2e26c140fc5
NewFieldForMe <newfield1001@gmail.com>
2017-01-29 01:11:01 +0900

    tsx compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var babelify = require('babelify');
 
 gulp.task('build', function() {
     browserify({
         entries: ['src/app.tsx']
     })
-    .plugin('tsify')
+    .plugin(tsify, { target: 'es6' })
+    .transform(babelify, { extensions: [ '.tsx', '.ts' ] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./dest/js'))
